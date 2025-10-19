914bcec8b1f233947a581e399871e71919da064a
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2025-10-19 19:06:57 -0400

    Add SukiSU-Ultra manager url and telegram link in thanks section

--- .github/scripts/kernel_release_post.py
+++ .github/scripts/kernel_release_post.py
@@ -34,7 +34,8 @@ def generate_kernel_post_message():
     msg += "<b>Samsung Galaxy S10 Series, Samsung Galaxy Note 10 Series & Samsung Galaxy F62</b>\n\n"
     msg += f"<a href='{RELEASE_URL}'>Kernel release</a>\n"
     msg += "<a href='https://github.com/ProjectMatrixx-exynos9820/android_kernel_samsung_exynos9820'>Kernel Source</a>\n"
-    msg += "<a href='https://github.com/KernelSU-Next/KernelSU-Next/releases'>KernelSU-Next Manager</a>\n\n"
+    msg += "<a href='https://github.com/KernelSU-Next/KernelSU-Next/releases'>KernelSU-Next Manager</a>\n"
+    msg += "<a href='https://github.com/SukiSU-Ultra/SukiSU-Ultra/releases'>SukiSU-Ultra Manager</a>\n\n"
 
     msg += f"<b>Changelog {current_date}:</b>\n"
     msg += f"{RELEASE_BODY}\n\n"
@@ -46,6 +47,7 @@ def generate_kernel_post_message():
     msg += "<a href='http://t.me/linux4'>linux4</a> for kernel\n"
     msg += "<a href='http://t.me/rifsxd'>rifsxd</a> for kernelsu-next\n"
     msg += "<a href='http://t.me/sidex15'>sidex15</a> for susfs4ksu\n"
+    msg += "<a href='http://t.me/ShirkNeko'>ShirkNeko</a> for SukiSU-Ultra\n"
     msg += "All testers"
 
     return msg
