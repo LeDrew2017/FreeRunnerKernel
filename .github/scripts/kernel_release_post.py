b28e85c662cd5dccb52295979868c2e329954b50
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2025-11-03 17:41:54 -0500

    Add Wild-KernelSU Manager link to release post

--- .github/scripts/kernel_release_post.py
+++ .github/scripts/kernel_release_post.py
@@ -36,6 +36,7 @@ def generate_kernel_post_message():
     msg += "<a href='https://github.com/ProjectMatrixx-exynos9820/android_kernel_samsung_exynos9820'>Kernel Source</a>\n"
     msg += "<a href='https://github.com/KernelSU-Next/KernelSU-Next/releases'>KernelSU-Next Manager</a>\n"
     msg += "<a href='https://github.com/SukiSU-Ultra/SukiSU-Ultra/releases'>SukiSU-Ultra Manager</a>\n\n"
+    msg += "<a href='https://github.com/WildKernels/Wild_KSU/releases'> Wild-KernelSU Manager</a>\n\n"
 
     msg += f"<b>Changelog {current_date}:</b>\n"
     msg += f"{RELEASE_BODY}\n\n"
